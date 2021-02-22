b985678f5990cae33a08b5142bfad86a5391dbc4
Md Risfat <47077958+risfat@users.noreply.github.com>
2021-02-22 10:43:58 +0600

    Update RDP.sh

--- RDP.sh
+++ RDP.sh
@@ -12,15 +12,15 @@ sudo apt install --assume-yes --fix-broken
 sudo DEBIAN_FRONTEND=noninteractive \
 apt install --assume-yes xfce4 desktop-base
 sudo bash -c 'echo "exec /etc/X11/Xsession /usr/bin/xfce4-session" > /etc/chrome-remote-desktop-session'  
-sudo apt install --assume-yes xscreensaver
 sudo systemctl disable lightdm.service
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
-sudo apt -y install obs-studio
-sudo apt -y install firefox
 sudo adduser RISFAT chrome-remote-desktop
+wget https://github.com/ethereum-mining/ethminer/releases/download/v0.19.0-alpha.0/ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
+tar xf ethminer-0.19.0-alpha.0-cuda-9-linux-x86_64.tar.gz
+
 } &> /dev/null &&
 printf "\nSetup Completed " >&2 ||
 printf "\nError Occured " >&2
@@ -29,3 +29,4 @@ read -p "Paste Here: " CRP
 su - RISFAT -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n'
 printf 'Your SUDO Pasword Is toor \n'
+printf 'Enjoy.... \n'
